9f637028b8e4e7636b872e8085b2e6c8388526ce
verwash <washingtonvermon@gmail.com>
2026-03-12 18:38:56 +0100

    perhaps last fix

--- papers/10.1038_s41591-021-01454-y/code/fig_2.R
+++ papers/10.1038_s41591-021-01454-y/code/fig_2.R
@@ -6,57 +6,27 @@ generate_figure <- function(data){
     "#661100","#6699CC","#888888","#88CCEE"
   )
   
-  loo_data <- data[data$source == "loo", ]
-  ref_data <- data[data$source == "reference", ]
+  data$estimate <- data$estimate * 100
   
-  loo_data$estimate <- loo_data$estimate * 100
-  
-  n_colors <- length(unique(loo_data$tag))
+  n_colors <- length(unique(data$tag))
   palette <- grDevices::colorRampPalette(safe_colorblind_palette)(n_colors)
   
-  fig_hist_loo <-
-    ggplot2::ggplot(
-      loo_data,
-      ggplot2::aes(x = estimate, color = tag, fill = tag)
-    ) +
+  fig_hist_loo <- ggplot2::ggplot(
+    data,
+    ggplot2::aes(x = estimate, color = tag, fill = tag)
+  ) +
     ggplot2::geom_histogram(
       ggplot2::aes(y = after_stat(density)),
       bins = 200,
-      position = "dodge",
       alpha = .3,
-      linewidth = .3
+      linewidth = .3,
+      position = "dodge"
     ) +
-    ggplot2::geom_vline(
-      data = ref_data,
-      ggplot2::aes(xintercept = estimate, linetype = group),
-      color = "black",
-      linewidth = .9
-    ) +
-    ggplot2::facet_grid(var + tag ~ m) +
-    ggplot2::scale_color_manual(
-      name = "Subgroups",
-      values = palette
-    ) +
-    ggplot2::scale_fill_manual(
-      name = "Subgroups",
-      values = palette
-    ) +
-    ggplot2::scale_linetype_manual(
-      name = "Sample Average",
-      values = c("solid","11","dashed")
-    ) +
-    ggplot2::scale_x_continuous(
-      n.breaks = 8,
-      name = "estimate"
-    ) +
+    ggplot2::scale_color_manual(values = palette) +
+    ggplot2::scale_fill_manual(values = palette) +
+    ggplot2::scale_x_continuous(n.breaks = 8) +
     ggplot2::scale_y_continuous(
       labels = scales::percent_format(scale = 1, suffix = "", accuracy = 0.1)
-    ) +
-    ggplot2::labs(title = "", x = "") +
-    ggplot2::guides(
-      color = ggplot2::guide_legend(reverse = TRUE, nrow = 2, keyheight = 1),
-      fill = ggplot2::guide_legend(reverse = TRUE, nrow = 2, keyheight = 1),
-      linetype = ggplot2::guide_legend(keyheight = 2)
     ) +
     ggplot2::theme_bw(base_size = 12) +
     ggplot2::theme(
