0ae55dff8a2fd32e92eff12f5674461ef17cefd4
cordmasche <cord.masche@student.hu-berlin.de>
2026-06-01 14:28:55 +0300

    update fig_2.R

--- papers/10.1371_journal.pone.0278337/code/fig_2.R
+++ papers/10.1371_journal.pone.0278337/code/fig_2.R
@@ -2,8 +2,8 @@ generate_figure <- function(data){
 
   main_results <-
     list(
-      cash = lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round, fixed_effects = ~id,  se_type = "stata", data = data) %>% tidy,
-      doses = lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round, fixed_effects = ~id,  se_type = "stata", data = data) %>% tidy) %>% bind_rows()
+      cash = estimatr::lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round, fixed_effects = ~id,  se_type = "stata", data = data) |> tidy,
+      doses = estimatr::lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round, fixed_effects = ~id,  se_type = "stata", data = data) |> tidy) |> dplyr::bind_rows()
 
 
   treatments <- c("trading_importance", "risk", "others_number", "others_giving")
@@ -13,12 +13,12 @@ generate_figure <- function(data){
 
   figure_2 <-
 
-    main_results %>%
-    dplyr::filter(term %in% treatments_norm) %>%
-    mutate(Treatment = factor(term, treatments_norm, treatment_labels),
-           outcome = factor(outcome, c("cash_billions", "doses"), c("Cash (billion Euros)", "Doses (Millions)"))) %>%
+    main_results |>
+    dplyr::filter(term %in% treatments_norm) |>
+    dplyr::mutate(Treatment = factor(term, treatments_norm, treatment_labels),
+           outcome = factor(outcome, c("cash_billions", "doses"), c("Cash (billion Euros)", "Doses (Millions)"))) |>
 
-    ggplot(aes(estimate, Treatment)) + geom_point()+
+    ggplot2::ggplot(aes(estimate, Treatment)) + geom_point()+
     geom_errorbar(aes(xmin = conf.low, xmax = conf.high), width = .1)+
     geom_vline(xintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
     theme_bw() + facet_grid(~outcome, scales = "free_x")+
