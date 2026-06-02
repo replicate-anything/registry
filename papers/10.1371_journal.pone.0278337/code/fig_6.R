c06d8818d947581cee50b2d327a93fa7d38e27ae
cordmasche <cord.masche@student.hu-berlin.de>
2026-06-02 12:26:59 +0300

    udpate functions

--- papers/10.1371_journal.pone.0278337/code/fig_6.R
+++ papers/10.1371_journal.pone.0278337/code/fig_6.R
@@ -6,29 +6,29 @@ generate_figure <- function(data){
   main_results_by_party <-
     lapply(unique(data$party), function(p)
       list(
-        cash = lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
+        cash = estimatr::lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                          fixed_effects = ~id,  se_type = "stata",
                          data = data |>  dplyr::filter(party == p)) |>  tidy,
-        doses = lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
+        doses = estimatr::lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                           fixed_effects = ~id,  se_type = "stata",
                           data = data |>  dplyr::filter(party == p)) |>  tidy) |> 
-        bind_rows() |>  mutate(party = p)) |> 
-    bind_rows()
+        dplyr::bind_rows() |>  dplyr::mutate(party = p)) |> 
+    dplyr::bind_rows()
 
   figure_1_party <-
 
     main_results_by_party |> 
     dplyr::filter(term %in% treatments_norm) |> 
-    mutate(Treatment = factor(term, treatments_norm, treatment_labels),
+    dplyr::mutate(Treatment = factor(term, treatments_norm, treatment_labels),
            outcome = factor(outcome, c("cash_billions", "doses"), c("Cash (billion Euros)", "Doses (Millions)"))) |> 
 
-    ggplot(aes(Treatment, estimate, color = party)) +
+    ggplot2::ggplot(ggplot2::aes(Treatment, estimate, color = party)) +
     geom_point(position = position_dodge(0.3))+
-    geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(0.3), width = .1)+
-    geom_hline(yintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
-    theme_bw() + facet_grid(~outcome, scales = "free_x")+
-    xlab("") +
-    coord_flip()
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = conf.low, ymax = conf.high), position = position_dodge(0.3), width = .1)+
+    ggplot2::geom_hline(yintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
+    ggplot2::theme_bw() + ggplot2::facet_grid(~outcome, scales = "free_x")+
+    ggplot2::xlab("") +
+    ggplot2::coord_flip()
 
   return(figure_1_party)
 }
--- papers/10.1371_journal.pone.0278337/code/fig_7.R
+++ papers/10.1371_journal.pone.0278337/code/fig_7.R
@@ -6,10 +6,10 @@ generate_figure <- function(data){
   main_results_by_background <-
     lapply(unique(data$migration_background), function(p)
       list(
-        cash = lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
+        cash = estimatr::lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                          fixed_effects = ~id,  se_type = "stata",
                          data = data |> dplyr::filter(migration_background == p)) |> broom::tidy,
-        doses = lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
+        doses = estimatr::lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                           fixed_effects = ~id,  se_type = "stata",
                           data = data |> dplyr::filter(migration_background == p)) |> broom::tidy) |>
         dplyr::bind_rows() |> dplyr::mutate(migration_background = factor(p))) |>
@@ -23,8 +23,8 @@ generate_figure <- function(data){
            outcome = factor(outcome, c("cash_billions", "doses"), c("Cash (billion Euros)", "Doses (Millions)"))) |>
 
     ggplot2::ggplot(ggplot2::aes(Treatment, estimate, color = migration_background)) +
-    ggplot2::geom_point(position = position_dodge(0.3))+
-    ggplot2::geom_errorbar(ggplot2::aes(ymin = conf.low, ymax = conf.high), position = position_dodge(0.3), width = .1)+
+    ggplot2::geom_point(position = ggplot2::position_dodge(0.3))+
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = conf.low, ymax = conf.high), position = ggplot2::position_dodge(0.3), width = .1)+
     ggplot2::geom_hline(yintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
     ggplot2::theme_bw() + ggplot2::facet_grid(~outcome, scales = "free_x")+
     ggplot2::xlab("") +
