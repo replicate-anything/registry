0605ece164132b9db889472596144d3cf3c12ea6
cordmasche <cord.masche@student.hu-berlin.de>
2026-06-02 12:36:13 +0300

    Update fig_7.R

--- papers/10.1371_journal.pone.0278337/code/fig_7.R
+++ papers/10.1371_journal.pone.0278337/code/fig_7.R
@@ -8,10 +8,10 @@ generate_figure <- function(data){
       list(
         cash = estimatr::lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                          fixed_effects = ~id,  se_type = "stata",
-                         data = data |> dplyr::filter(migration_background == p)) |> broom::tidy,
+                         data = data |> dplyr::filter(migration_background == p)) |> broom::tidy(),
         doses = estimatr::lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                           fixed_effects = ~id,  se_type = "stata",
-                          data = data |> dplyr::filter(migration_background == p)) |> broom::tidy) |>
+                          data = data |> dplyr::filter(migration_background == p)) |> broom::tidy()) |>
         dplyr::bind_rows() |> dplyr::mutate(migration_background = factor(p))) |>
     dplyr::bind_rows()
 
@@ -25,7 +25,7 @@ generate_figure <- function(data){
     ggplot2::ggplot(ggplot2::aes(Treatment, estimate, color = migration_background)) +
     ggplot2::geom_point(position = ggplot2::position_dodge(0.3))+
     ggplot2::geom_errorbar(ggplot2::aes(ymin = conf.low, ymax = conf.high), position = ggplot2::position_dodge(0.3), width = .1)+
-    ggplot2::geom_hline(yintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
+    ggplot2::geom_hline(yintercept = 0, linetype = "longdash", lwd = 0.35, colour = "#B55555") +
     ggplot2::theme_bw() + ggplot2::facet_grid(~outcome, scales = "free_x")+
     ggplot2::xlab("") +
     ggplot2::coord_flip()
