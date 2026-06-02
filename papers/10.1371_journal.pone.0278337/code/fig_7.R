5d227c74719df7398b05c03ae1afccd00138b774
cordmasche <cord.masche@student.hu-berlin.de>
2026-06-02 12:20:19 +0300

    Update fig_7.R

--- papers/10.1371_journal.pone.0278337/code/fig_7.R
+++ papers/10.1371_journal.pone.0278337/code/fig_7.R
@@ -8,27 +8,27 @@ generate_figure <- function(data){
       list(
         cash = lm_robust(cash_billions ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                          fixed_effects = ~id,  se_type = "stata",
-                         data = data %>% dplyr::filter(migration_background == p)) %>% tidy,
+                         data = data |> dplyr::filter(migration_background == p)) |> broom::tidy,
         doses = lm_robust(doses ~ trading_importance*risk*others_number_norm*others_giving_norm  + round,
                           fixed_effects = ~id,  se_type = "stata",
-                          data = data %>% dplyr::filter(migration_background == p)) %>% tidy) %>%
-        bind_rows() %>% mutate(migration_background = factor(p))) %>%
-    bind_rows()
+                          data = data |> dplyr::filter(migration_background == p)) |> broom::tidy) |>
+        dplyr::bind_rows() |> dplyr::mutate(migration_background = factor(p))) |>
+    dplyr::bind_rows()
 
   figure_1_background <-
 
-    main_results_by_background %>%
-    dplyr::filter(term %in% treatments_norm) %>%
-    mutate(Treatment = factor(term, treatments_norm, treatment_labels),
-           outcome = factor(outcome, c("cash_billions", "doses"), c("Cash (billion Euros)", "Doses (Millions)"))) %>%
+    main_results_by_background |>
+    dplyr::filter(term %in% treatments_norm) |>
+    dplyr::mutate(Treatment = factor(term, treatments_norm, treatment_labels),
+           outcome = factor(outcome, c("cash_billions", "doses"), c("Cash (billion Euros)", "Doses (Millions)"))) |>
 
-    ggplot(aes(Treatment, estimate, color = migration_background)) +
-    geom_point(position = position_dodge(0.3))+
-    geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(0.3), width = .1)+
-    geom_hline(yintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
-    theme_bw() + facet_grid(~outcome, scales = "free_x")+
-    xlab("") +
-    coord_flip()
+    ggplot2::ggplot(ggplot2::aes(Treatment, estimate, color = migration_background)) +
+    ggplot2::geom_point(position = position_dodge(0.3))+
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = conf.low, ymax = conf.high), position = position_dodge(0.3), width = .1)+
+    ggplot2::geom_hline(yintercept=0, linetype="longdash", lwd=0.35, colour = "#B55555") +
+    ggplot2::theme_bw() + ggplot2::facet_grid(~outcome, scales = "free_x")+
+    ggplot2::xlab("") +
+    ggplot2::coord_flip()
 
   return(figure_1_background)
 }
