44ba3c12f39c3900d46a13c4921cdf90178e94ae
cordmasche <cord.masche@student.hu-berlin.de>
2026-06-03 13:01:51 +0300

    update fig_3.R

--- papers/10.1038_s41591-021-01454-y/code/fig_3.R
+++ papers/10.1038_s41591-021-01454-y/code/fig_3.R
@@ -1,24 +1,31 @@
 generate_figure <- function(data){
+  library(dplyr)
+  library(ggplot2)
 
-  fig_2 <-
-    data %>%
-    dplyr::filter(!is.na(n_sub)) %>%
-    dplyr::mutate(name = plyr::mapvalues(name, "All", "All LMICs")) %>%
-    ggplot(aes(name, estimate, color = tag)) +
-    geom_errorbar(aes(ymin = conf.low, ymax = conf.high),
+  safe_colorblind_palette <- c(
+  "#CC6677","#DDCC77","#117733","#332288",
+  "#AA4499","#44AA99","#999933","#882255",
+  "#661100","#6699CC","#888888","#88CCEE")
+  
+  fig_3 <-
+    data |>
+    dplyr::filter(!is.na(n_sub)) |>
+    dplyr::mutate(name = plyr::mapvalues(name, "All", "All LMICs")) |>
+    ggplot2::ggplot(aes(name, estimate, color = tag)) +
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = conf.low, ymax = conf.high),
                   size = .5, width = .2, position = position_dodge(0.6)) +
-    geom_point(shape = 16, position = position_dodge(0.6)) +
-    facet_grid(.~tag,  space = "free", labeller = label_wrap_gen(width = 15)) +
-    scale_size_discrete(range = c(1,3), name = "Number of observations" ) +
-    geom_vline(xintercept = 3.5, color = "darkgrey") +
-    geom_vline(xintercept = 2.5, color = "darkgrey") +
-    guides(color = FALSE) +
-    scale_colour_manual(values = safe_colorblind_palette) +
-    coord_flip() + theme_bw() +
-    labs(title = "Why would you not take the COVID-19 vaccine?",
+    ggplot2::geom_point(shape = 16, position = ggplot2::position_dodge(0.6)) +
+    ggplot2::facet_grid(.~tag,  space = "free", labeller = label_wrap_gen(width = 15)) +
+    ggplot2::scale_size_discrete(range = c(1,3), name = "Number of observations" ) +
+    ggplot2::geom_vline(xintercept = 3.5, color = "darkgrey") +
+    ggplot2::geom_vline(xintercept = 2.5, color = "darkgrey") +
+    ggplot2::guides(color = FALSE) +
+    ggplot2::scale_colour_manual(values = safe_colorblind_palette) +
+    ggplot2::coord_flip() + ggplot2::theme_bw() +
+    ggplot2::labs(title = "Why would you not take the COVID-19 vaccine?",
          x = "") +
-    theme_bw() + ylim(c(-2,100)) +
-    theme(legend.position = "bottom",
+    ggplot2::theme_bw() + ggplot2::ylim(c(-2,100)) +
+    ggplot2::theme(legend.position = "bottom",
           plot.caption = element_text(hjust = 0), #Default is hjust=1
           plot.title.position = "plot", #NEW parameter. Apply for subtitle too.
           plot.caption.position =  "plot",
