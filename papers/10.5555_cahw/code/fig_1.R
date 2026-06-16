40b3668d2100bf3ac1f10ea0a02a1113861cc2e6
cordmasche <cord.masche@student.hu-berlin.de>
2026-06-16 18:22:06 +0200

    Update fig_1.R

--- papers/10.5555_cahw/code/fig_1.R
+++ papers/10.5555_cahw/code/fig_1.R
@@ -3,19 +3,19 @@ generate_figure <- function(data){
   vs <- c("y_effort_2", "n_reports_2_std", "n_animals_2_std", "findable_2_std")
   
   fig_2 <- 
-    data |> select(all_of(vs)) |> 
-    gather(var, val) |>
-    filter(var %in% vs) |> 
-    mutate(
+    data |> dplyr::select(all_of(vs)) |> 
+    dplyr::gather(var, val) |>
+    dplyr::filter(var %in% vs) |> 
+    dplyr::mutate(
       var = factor(var, vs, 
                    c("CAHW performance index", 
                      "Reports submitted", 
                      "Animals examined", 
                      "Findable CAHW"))) |>
-    ggplot(aes(val)) + 
-    geom_histogram(boundary = 0, closed = "left", bins = 25)  + 
-    facet_wrap(~var, scales = "free") + 
-    theme_bw() + 
+    ggplot2::ggplot(aes(val)) + 
+    ggplot2::geom_histogram(boundary = 0, closed = "left", bins = 25)  + 
+    ggplot2::facet_wrap(~var, scales = "free") + 
+    ggplot2::theme_bw() + 
     labs(x = NULL)
   
   return(fig_2)
